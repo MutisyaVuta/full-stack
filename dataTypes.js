773f82411f2c941b78328c18a3107531a086ff1d
Nicholas Mutisya <nicholas.mutisya77@gmail.com>
2025-09-19 11:19:00 +0300

    lenth

--- dataTypes.js
+++ dataTypes.js
@@ -3,7 +3,19 @@ String interpolation.You can create strings using Template Literals.
 Template literals allow you to embed expressions within backticks (`) for dynamic string creation, 
 making it more readable and versatile.
 */
-let s = 'Javascript'
-let s1 = `You are learning ${s}`;
+// let s = 'Javascript'
+// let s1 = `You are learning ${s}`;
 
-console.log(s1)
+// console.log(s1)
+
+//String concatenation
+
+let s3 = 'Java';
+let s4 = 'Script';
+
+let fullname = s3+s4;
+
+let len = fullname.length
+
+console.log(fullname)
+console.log('The length for fullname is:' + len)
