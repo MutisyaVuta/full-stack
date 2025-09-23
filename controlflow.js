79fe8c222122ec6ab3d083ba4c8ba3ecd893000d
Nicholas Mutisya <nicholas.mutisya77@gmail.com>
2025-09-23 11:52:48 +0300

    contol flow

---
+++ controlflow.js
@@ -0,0 +1,16 @@
+//Javascript if statement- excecutes a block of code only if a specified condtion is true 
+// const age = 18
+// if (age >= 18) {                        //Checks if age is greater than or equal to 18.
+//     console.log('You are an adult')     //Logs "You are an adult." if the condition is true.
+// }
+
+//JavaScript if...else Statement
+//The if...else statement provides an alternate block of code to execute if the condition is false.
+
+let mark = 39;
+if (mark >= 40) {
+    console.log('You have passed')
+}
+else {
+    console.log('You have failed')
+}
---
+++ todaysClass.js
@@ -0,0 +1,53 @@
+//how to check type of data type
+// let x;
+// console.log(typeof x);
+
+// const name1 = 'Nicholas'
+
+// console.log(typeof name1)
+
+// let number1 = 2
+
+// console.log(typeof number1)
+
+/*Adding Numbers and Strings
+JavaScript uses the `+` operator for both addition and concatenation.
+Numbers are added, when strings are concatenated.*/
+
+//JavaScript uses the `+` operator for both addition and concatenation.
+//Numbers are added, when strings are concatenated.
+
+//Adding numbers
+// let a =4;
+// let b = 5;
+// c = a + b;
+
+// console.log(c)
+
+// let d = '10'
+// let e = '20'
+// g = 'Hello'
+
+// f = d+e;
+// h= e+b
+
+// z = g + b
+// console.log(z)
+
+
+// console.log(f)
+// console.log(h)
+
+
+/*Numeric Strings
+JavaScript automatically converts the numeric strings to numbers in most operations like. */
+let divide1 ='100' / '10'
+let n = '100' * '10'
+let m = '100' - '10'
+let p = '100' + '10'
+
+console.log(divide1)
+console.log(n)
+
+console.log(p)
+console.log(m)
