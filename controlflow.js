d421fec54a1fb8db1084b0b8e03e78e91d6f9299
Nicholas Mutisya <nicholas.mutisya77@gmail.com>
2025-09-23 20:04:32 +0300

    grade evaluator

--- controlflow.js
+++ controlflow.js
@@ -16,12 +16,13 @@
 //The if-else statement allows you to execute one block of code if the condition is true and another block if the condition is false. 
 It's a way to handle two possible outcomes based on a condition.*/
 
-let mark = 39;
-if (mark >= 40) {
-    console.log('You have passed')
-}
-else {
-    console.log('You have failed')
+// let mark = 39;
+// if (mark >= 40) {
+//     console.log('You have passed')
+// }
+// else {
+//     console.log('You have failed')
+// }
 
 
 // More example s
@@ -31,21 +32,21 @@ else {
 // else
 //     console.log("I am Not in if");
 
-let age = 16;
+// let age = 16;
 
-if (age >= 18) {
-    console.log("You are an adult.");
-} else {
-    console.log("You are a minor.");
-}
+// if (age >= 18) {
+//     console.log("You are an adult.");
+// } else {
+//     console.log("You are a minor.");
+// }
 
 
 //JavaScript nested-if statement
 // JavaScript lets you put an if statement inside another if statement. This is called a nested if, where one if or else is inside another.
 
 // illustration
-let i = 10;
-if (i == 10){ //First if statement
+let i = 11;
+if (i == 11){ //First if statement
     if(i < 15 ){
         console.log('i is smaller than 15 ')
         //Nested if statement
@@ -78,15 +79,15 @@ if (i == 10){ //First if statement
 
 // let temp = 9;
 
-// if (temp >= 30) {
-//     console.log("It's hot outside.");
-// } else if (temp >= 20) {
-//     console.log("It's warm outside.");
-// } else if (temp >= 10) {
-//     console.log("It's cool outside.");
-// } else {
-//     console.log("It's cold outside.");
-// }
+if (temp >= 30) {
+    console.log("It's hot outside.");
+} else if (temp >= 20) {
+    console.log("It's warm outside.");
+} else if (temp >= 10) {
+    console.log("It's cool outside.");
+} else {
+    console.log("It's cold outside.");
+}
 
 // let score = 88;
 
