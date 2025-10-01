17f3562a90305bc260aed540bee14abb34166177
Nicholas Mutisya <nicholas.mutisya77@gmail.com>
2025-10-01 12:07:13 +0300

    functions

---
+++ functionTrainBall.js
@@ -0,0 +1,9 @@
+function trainFootball (warmUp = 'Jog', mainDrill= 'Small sided game'){
+
+    console.log(`Get in the field and ${warmUp} before the ${mainDrill}`)
+
+    
+}
+trainFootball(warmUp = 'Jumps and sprints',mainDrill= 'Scoring goals practice')
+
+
