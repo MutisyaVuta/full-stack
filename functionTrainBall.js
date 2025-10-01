e34e3476e774bd7be23249a6ae66e268cfcdb2e8
Nicholas Mutisya <nicholas.mutisya77@gmail.com>
2025-10-01 12:15:23 +0300

    added return value

--- functionTrainBall.js
+++ functionTrainBall.js
@@ -1,9 +1,19 @@
+
+const weatherToday = 'Rainy'
+
 function trainFootball (warmUp = 'Jog', mainDrill= 'Small sided game'){
+
+    if (weatherToday== 'Rainy'){
+        return `The weather is not favourable to train`
+    }
+    else{
+        return `Training will happen. It will start with ${warmUp} and then ${mainDrill}`
+    }
 
     console.log(`Get in the field and ${warmUp} before the ${mainDrill}`)
 
     
 }
-trainFootball(warmUp = 'Jumps and sprints',mainDrill= 'Scoring goals practice')
+const trainStatus= trainFootball(warmUp = 'Jumps and sprints',mainDrill= 'Scoring goals practice')
 
-
+console.log(trainStatus)
