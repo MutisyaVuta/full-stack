ddeff29165adde7f67ab5449f0011c0709def33f
Nicholas Mutisya <nicholas.mutisya77@gmail.com>
2025-10-01 13:13:32 +0300

    ddd

--- functionTrainBall.js
+++ functionTrainBall.js
@@ -1,19 +1,43 @@
 
-const weatherToday = 'Rainy'
+// const weatherToday = 'Cloudy'
 
-function trainFootball (warmUp = 'Jog', mainDrill= 'Small sided game'){
+// function trainFootball (warmUp = 'Jog', mainDrill= 'Small sided game'){
 
-    if (weatherToday== 'Rainy'){
-        return `The weather is not favourable to train`
-    }
-    else{
-        return `Training will happen. It will start with ${warmUp} and then ${mainDrill}`
-    }
+//     if (weatherToday== 'Rainy'){
+//         return `The weather is not favourable to train`
+//     }
+//     else{
+//         return `Training will happen. It will start with ${warmUp} and then ${mainDrill}`
+//     }
 
-    console.log(`Get in the field and ${warmUp} before the ${mainDrill}`)
+//     console.log(`Get in the field and ${warmUp} before the ${mainDrill}`)
 
     
-}
-const trainStatus= trainFootball(warmUp = 'Jumps and sprints',mainDrill= 'Scoring goals practice')
+// }
+// const trainStatus= trainFootball(warmUp = 'Jogging',mainDrill= 'Defending drills')
 
-console.log(trainStatus)
+// console.log(trainStatus)
+
+//funcion declaration and calling
+// function razzle (){
+//     console.log('You have been razzled!')
+// }
+// razzle();
+
+
+//You can call functions after you call them
+// razzle();
+// function razzle(){
+//     console.log('You have been razzled!')
+// }
+
+//functions can be passed arguments
+function razzle (lawyer = 'Billy', target = 'em'){
+    console.log(`${lawyer} razzle-dazzeles ${target}!`);
+}
+razzle();
+razzle('Nick', 'Tchallah');
+
+
+
+
