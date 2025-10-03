ec727cec5f060fc11879fc9b7aacf146e5ab681e
Nicholas Mutisya <nicholas.mutisya77@gmail.com>
2025-10-03 15:28:31 +0300

    more advanced

--- callBackFunctions.js
+++ callBackFunctions.js
@@ -16,17 +16,34 @@
 
 
 //function to return values inside a function
-function iInvokeThings(thing){
-return thing()
-}
-resultsNumber = iInvokeThings(function(){return 4+5});
-resultsStrting = iInvokeThings(function(){ return 'Hello' + 'World'});
- console.log(resultsNumber)
- console.log(resultsStrting)
+// function iInvokeThings(thing){
+// return thing()
+// }
+// resultsNumber = iInvokeThings(function(){return 4+5});
+// resultsStrting = iInvokeThings(function(){ return 'Hello' + 'World'});
+//  console.log(resultsNumber)
+//  console.log(resultsStrting)
 
- //call back function
+//  //call back function
 
- function main (cb){
-    console.log(cb());
+//  function main (cb){
+//     console.log(cb());
+//  }
+ //main(function(){return "After I get passed to the main() function as the only argument, I'm stored in the local 'cb' variable!"})
+
+ //parameter defined for outer functions are available anywhere inside the function
+ //As a result, we can pass them as arguments to the callback function.
+
+ function greet (name,cb){
+    return cb(name);
  }
- main(function(){return "After I get passed to the main() function as the only argument, I'm stored in the local 'cb' variable!"})
+ output = greet('Ada Lovelace', function(name){return 'Hello there,' + name});
+
+ console.log(output)
+
+ function doMath(num1,num2, cb){
+    return cb(num1,num2);
+ }
+ answer = doMath(6,8, function(num1,num2){return num1 * num2})
+ console.log(answer)
+
