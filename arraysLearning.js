2867871ab1159168616007ac67b7c23e8ca99aab
Nicholas Mutisya <nicholas.mutisya77@gmail.com>
2025-10-08 15:44:24 +0300

    h

--- arraysLearning.js
+++ arraysLearning.js
@@ -1,6 +1,51 @@
 //display how effective arrays are in keeping data organized
-const winningNumbers = [2, 4, 6, 8, 10];
-function logWinningNumbers(numbers) {
-    console.log("Winning numbers are:", numbers);
-}
-logWinningNumbers(winningNumbers);
+// const winningNumbers = [2, 4, 6, 8, 10];
+// function logWinningNumbers(numbers) {
+//     console.log("Winning numbers are:", numbers);
+// }
+// logWinningNumbers(winningNumbers);
+
+// console.log(winningNumbers);
+
+//accessing elements in an array
+
+// const winningNumbers = [2, 4, 6, 8, 10];
+// winningNumbers[0];
+
+// console.log(winningNumbers[0]);
+// console.log(winningNumbers[1]);
+// console.log(winningNumbers[2]);
+// console.log(winningNumbers[3]);
+// console.log(winningNumbers[4]);
+
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+alphabet.length
+console.log(alphabet.length);
+console.log(alphabet[alphabet.length - 1]);
