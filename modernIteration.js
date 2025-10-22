a7e26d2017edd131ee9ee57862ccac0483e01cf1
Nicholas Mutisya <nicholas.mutisya77@gmail.com>
2025-10-22 08:50:41 +0300

    for in

--- modernIteration.js
+++ modernIteration.js
@@ -12,7 +12,19 @@
 //     console.log(char);
 // }
 
-const myName = 'Nicholas Mutisya';
-for(const char of myName){
-    console.log(char);
+// const myName = 'Nicholas Mutisya';
+// for(const char of myName){
+//     console.log(char);
+// }
+
+//for...in
+const address = {
+    street1: '6143 Ruiru',
+    street2: 'P.O. Box 12345',
+    city: 'Nairobi',
+    state: 'Kenya',
+    zip: '00100'
+}
+for (const key in address) {
+    console.log(address[key])
 }
