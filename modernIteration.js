318132a8c7643afd29a15107a479a3e2c59e7edc
Nicholas Mutisya <nicholas.mutisya77@gmail.com>
2025-10-22 14:09:31 +0300

    for..of limit

--- modernIteration.js
+++ modernIteration.js
@@ -5,6 +5,12 @@
 //     console.log(`Hello, ${item}!`);     
 // }
 
+//for ...of limitations with arrays
+const colours = ['red', 'green', 'blue'];
+for (const [index, color] of colours.entries()) {
+    console.log(`Colour at index ${index} is: ${color}`);
+}
+
 //string iteration
 // const greeting = "Hello!";
 // for (const char of greeting) {
@@ -18,13 +24,24 @@
 // }
 
 //for...in
-const address = {
-    street1: '6143 Ruiru',
-    street2: 'P.O. Box 12345',
-    city: 'Nairobi',
-    state: 'Kenya',
-    zip: '00100'
-}
-for (const key in address) {
-    console.log(address[key])
-}
+// const address = {
+//     street1: '6143 Ruiru',
+//     street2: 'P.O. Box 12345',
+//     city: 'Nairobi',
+//     state: 'Kenya',
+//     zip: '00100'
+// }
+// for (const key in address) {
+//     if(address.hasOwnProperty(key))
+//         console.log(`${key}: ${address[key]}`);
+//     // console.log(address[key])
+// }
+
+// const user = { name: "Jen", role: "Developer" };
+
+// for (const key in user) {
+//   if (user.hasOwnProperty(key)) {
+//     console.log(`${key}: ${user[key]}`);
+//   }
+// }
+
